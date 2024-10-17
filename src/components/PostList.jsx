5773ff1250ededa33f94f6b1917a415dbbfecfd5
Shaiful191 <saifulislamridoy1999@gmail.com>
2024-10-17 09:05:55 +0600

    useEffect added

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,29 +1,45 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Post from './Post'
 import { PostList as PostListData } from '../store/post-list-store';
 import WelcomeMsg from './WelcomeMsg';
+import LoadingSpinner from './LoadingSpinner';
 
 const PostList = () => {
 
   const { postList, addInitialPost } = useContext(PostListData);
 
-  const handleGetPostsClick = () => {
-    fetch('https://dummyjson.com/posts')
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch('https://dummyjson.com/posts', { signal })
       .then(res => res.json())
       .then((data) => {
-        console.log(data.posts);
+        // console.log(data.posts);
         addInitialPost(data.posts);
+        setFetching(false);
       })
       .catch((error) => {
         console.error("Error fetching posts:", error);
       });
-  };
+    return () => {
+      //  console.log("Cleaning up UseEffect.");
+      controller.abort();
+    }
+  }, []);
+
 
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMsg onGetPostsClick={handleGetPostsClick} />}
-      {
+      {/* Loading state */}
+      {fetching && <LoadingSpinner />}
+
+      {!fetching && postList.length === 0 && <WelcomeMsg />}
+      {!fetching &&
         postList.map((post) => (
           <Post key={post.id} post={post} />
         ))
--- src/components/WelcomeMsg.jsx
+++ src/components/WelcomeMsg.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const WelcomeMsg = ({onGetPostsClick}) => {
+const WelcomeMsg = () => {
   return (
     <center className='welcome-msg'>
       <h1 >There are no post</h1>
-      <button onClick={onGetPostsClick} type="button" className="btn btn-primary">Get Posts From  Server</button>
     </center>
   )
 }
